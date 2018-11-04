25228789ba04d9dfca20a2b194143f97b08685be
Administrator <Administrator@DESKTOP-80FIM1S>
2018-11-04 20:51:53 +0800

    在吃到食物之后根据食物的节数来对蛇身进行增长

--- 贪吃蛇_重构/snake.cpp
+++ 贪吃蛇_重构/snake.cpp
@@ -138,20 +138,24 @@ bool Snake::isEatFood(Food food)
 	//如果吃到了
 	if (pos.front().first == food.getX() && pos.front().second == food.getY())
 	{
-		//获取尾巴
-		std::pair<int, int> last = pos.back();
-		//获取倒数第二节
-		pos.pop_back();
-		std::pair<int, int> lastSecond = pos.back();
-		//重新把尾巴安上去
-		pos.push_back(last);
-		//新的尾巴
-		std::pair<int, int> newLast{};
-		//计算新的尾巴的位置
-		newLast.first = last.first + last.first - lastSecond.first;
-		newLast.second = last.second + last.second - lastSecond.second;
-		//安装
-		pos.push_back(newLast);
+		//吃了这个食物之后蛇身增加多少节
+		for (int i = 0; i < food.getSection(); i++)
+		{
+			//获取尾巴
+			std::pair<int, int> last = pos.back();
+			//获取倒数第二节
+			pos.pop_back();
+			std::pair<int, int> lastSecond = pos.back();
+			//重新把尾巴安上去
+			pos.push_back(last);
+			//新的尾巴
+			std::pair<int, int> newLast{};
+			//计算新的尾巴的位置
+			newLast.first = last.first + last.first - lastSecond.first;
+			newLast.second = last.second + last.second - lastSecond.second;
+			//安装
+			pos.push_back(newLast);
+		}
 		return true;
 	}
 	else
