067694b4bbc90024478971651818c51b00e07050
1m188 <228117330@qq.com>
2020-02-06 14:16:55 +0800

    修正语法错误

--- snake_c/src/ql/ql.h
+++ snake_c/src/ql/ql.h
@@ -5,5 +5,5 @@
 
 // snake move with Q-Learning algorithm
 // input parameter the snake struct instance, food struct instance, left, right, up, down limits
-// return the key will be press
+// return the key will be pressed
 const char qlMove(const Snake *const snake, const Food *const food, const int leftLim, const int rightLim, const int upLim, const int downLim);
