8ca35f62e1499f3e57bba5b9c0887128a84c7f94
1m188 <228117330@qq.com>
2020-02-20 21:07:20 +0800

    添加双缓冲绘制和帧率控制

--- snake_pygame/main.py
+++ snake_pygame/main.py
@@ -3,7 +3,14 @@ import pygame
 
 screen = pygame.display.set_mode((500, 500))
 
+clock = pygame.time.Clock()
+
 while True:
+    clock.tick_busy_loop(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
